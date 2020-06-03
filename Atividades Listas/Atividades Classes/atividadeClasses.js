1cc8fb4e8930eb132aba52c72c0292e58b648643
kaxkata <gabrielr387@gmail.com>
2020-06-02 21:44:47 -0300

    atualizacao exercicio classe 01

--- Atividades Listas/Atividades Classes/atividadeClasses.js
+++ Atividades Listas/Atividades Classes/atividadeClasses.js
@@ -8,6 +8,22 @@ var rs = require("readline-sync")
          this.modelo = modelo;
          this.cor = cor;
      }
+}
 
-    
+function criarCarro(){
+     var carro = new Carro();
+     carro.modelo = rs.question("Diga o nome do carro").toLowerCase;
+     carro.marca = rs.question("Diga o nome da marca").toLowerCase;
+     carro.cor = rs.question("Diga a cor do carro").toLowerCase;
+     return carro;
+ }
+
+ var carro1 = criarCarro();
+ var carro2 = criarCarro();
+
+
+ if(carro1.marca == carro2.marca && carro1.modelo == carro2.modelo){
+     console.log("Os carros são iguais");
+ } else {
+     console.log("Os carros são diferentes")
  }
